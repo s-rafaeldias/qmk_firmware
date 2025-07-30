ad00d3e15b82ace88d2915a50141076614f8f93c
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2025-07-30 21:32:41 +0200

    Add vim symbols layer

--- keyboards/bluebell/swoop/keymaps/s_rafaeldias/keymap.c
+++ keyboards/bluebell/swoop/keymaps/s_rafaeldias/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   HRM_A,    HRM_S,   HRM_D,     HRM_F,          KC_G,    /*|*/    KC_H,    HRM_J,       HRM_K,          HRM_L,      HRM_SCLN,
   KC_Z,     KC_X,    KC_C,      KC_V,           KC_B,    /*|*/    KC_N,    KC_M,        KC_COMM,        KC_DOT,     KC_SLSH,
 
-                     SYM_ESC,   MO(_SYMBOLS),   KC_SPC,  /*|*/    KC_ENT,  KC_BSPC,     MO(_NUMBERS)
+                     MO(_VIM),  MO(_SYMBOLS),   KC_SPC,  /*|*/    KC_ENT,  KC_BSPC,     MO(_NUMBERS)
 ),
 [_NUMBERS] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,      KC_4,     KC_5,          KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
