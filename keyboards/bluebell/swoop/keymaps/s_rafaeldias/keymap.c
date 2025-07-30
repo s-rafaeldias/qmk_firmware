7e5233957fc1195d8446a627558b3bf06b7898d1
Rafael Dias Silveira <s.rafaeldias@protonmail.com>
2025-07-30 21:35:55 +0200

    Add vim symbols

--- keyboards/bluebell/swoop/keymaps/s_rafaeldias/keymap.c
+++ keyboards/bluebell/swoop/keymaps/s_rafaeldias/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *              `---------------------'     `-----------------------'
  */
 [_HOMEROWMOD] = LAYOUT_split_3x5_3(
-  KC_Q,     KC_W,    KC_E,      KC_R,           KC_T,    /*|*/    KC_Y,    KC_U,        KC_I,           KC_O,       KC_P,
-  HRM_A,    HRM_S,   HRM_D,     HRM_F,          KC_G,    /*|*/    KC_H,    HRM_J,       HRM_K,          HRM_L,      HRM_SCLN,
-  KC_Z,     KC_X,    KC_C,      KC_V,           KC_B,    /*|*/    KC_N,    KC_M,        KC_COMM,        KC_DOT,     KC_SLSH,
+  KC_Q,     KC_W,    KC_E,      KC_R,           KC_T,    /*|*/    KC_Y,     KC_U,        KC_I,           KC_O,       KC_P,
+  HRM_A,    HRM_S,   HRM_D,     HRM_F,          KC_G,    /*|*/    KC_H,     HRM_J,       HRM_K,          HRM_L,      HRM_SCLN,
+  KC_Z,     KC_X,    KC_C,      KC_V,           KC_B,    /*|*/    KC_N,     KC_M,        KC_COMM,        KC_DOT,     KC_SLSH,
 
-                     MO(_VIM),  MO(_SYMBOLS),   KC_SPC,  /*|*/    KC_ENT,  KC_BSPC,     MO(_NUMBERS)
+                     MO(_VIM),  MO(_SYMBOLS),   KC_SPC,  /*|*/    KC_BSPC,  KC_ENT,     MO(_NUMBERS)
 ),
 [_NUMBERS] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,      KC_4,     KC_5,          KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
